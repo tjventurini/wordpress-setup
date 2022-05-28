f9fda079b4e8459ddff7381000988e9eca88c5b2
Thomas Venturini <thomas@venturini.codes>
2022-05-29 01:48:10 +0200

    another attempt

--- scripts/remote-setup.sh
+++ scripts/remote-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-read -r "Enter project name please: " PROJECT_NAME
+read -p "Enter project name please: " PROJECT_NAME
 echo ""
 git clone https://github.com/tjventurini/wordpress-setup.git $PROJECT_NAME
 cd PROJECT_NAME
