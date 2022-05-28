264025b8ba8f059e036c7951a9080cb92b6fdb7c
Thomas Venturini <thomas@venturini.codes>
2022-05-29 01:19:55 +0200

    try out another selection for remote setup

--- scripts/remote-setup.sh
+++ scripts/remote-setup.sh
@@ -7,17 +7,19 @@ WARN='\033[0;33m'
 NC='\033[0m' # No Color
 
 # Ask if this is the correct directory
-echo -e "${WARN}Installation will happen in $(pwd). Is that ok?${NC}"
-select yn in "yes" "no"; do
+while true; do
+    read -p "${WARN}Installation will happen in $(pwd), ok? [y|n|yes|no]${NC}"
     case $yn in
-        yes)
+        y|yes)
             echo -e "${SUCCESS}Alright, let's go! 🚀${NC}"
             break
             ;;
-        *)
+        n|no)
             echo -e "${WARN}Ok. Please create the directory ('mkdir <project-dir>'), navigate into it ('cd <project-dir>') and run this script again.${NC}"
             exit 1
-            break
+            ;;
+        *)
+            echo -e "${WARN}Please answer yes or no.${NC}"
             ;;
     esac
 done
