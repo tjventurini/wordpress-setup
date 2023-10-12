8da3e0c80bc16de21c710acb43cda3f7afe742d6
Thomas Venturini <thomas@venturini.codes>
2023-10-13 00:36:01 +0200

    remove parameters (never used them)

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,24 +8,6 @@ function showHelp() {
     echo -e "${WARN}Usage (Makefile): make init${NC}"
 }
 
-# Get the correct environment if given.
-if [ ! -z "$1" ]; then
-    case "$1" in
-        --prod)
-            ENVIRONMENT="prod"
-            break
-            ;;
-        --local)
-            ENVIRONMENT="local"
-            break
-            ;;
-        *)
-            showHelp
-            exit 1
-            ;;
-    esac
-fi
-
 # Let the user select the environment if not given.
 if [ -z "$ENVIRONMENT" ]; then
     echo -e "${WARN}Select environment:${NC}"
